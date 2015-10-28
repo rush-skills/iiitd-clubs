91a806b684158c402661f72f0df5b53761420bb1
Ankur Singh <ankur13019@iiitd.ac.in>
2015-10-28 21:43:50 +0530

    Bugfixes

--- clubs.rb
+++ clubs.rb
@@ -21,7 +21,7 @@ descriptions = {
 
 iframe1 = '<head><title>'
 iframe2 = ' IIIT-Delhi\'s '
-iframe3 = ' Club</title><link rel="shortcut icon" href="favicon.png?v=2" /><meta http-equiv="Content-Type" content="text/html\; charset=utf-8" /><style type="text/css">body, html{margin: 0\; padding: 0\; height: 100\%\; overflow: hidden\;}#content{position:absolute\; left: 0\; right: 0\; bottom: 0\; top: 0px\; }</style></head><body><div id="content"><iframe width="100\%" height="100\%" frameborder="0" src="'
+iframe3 = ' Club</title><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><style type="text/css">body, html{margin: 0; padding: 0; height: 100%; overflow: hidden;}#content{position:absolute; left: 0; right: 0; bottom: 0; top: 0px; }</style></head><body><div id="content"><iframe width="100%" height="100%" frameborder="0" src="'
 iframe4 = '" /></div></body></html>'
 
 base_dir = "./club/"
@@ -29,7 +29,7 @@ iframes.each do |key,url|
 	desc = descriptions[key]
 	output_html = [iframe1,key,iframe2,desc,iframe3,url,iframe4].join('')
 	dir = Dir.mkdir(base_dir+key.to_s.downcase) unless Dir.exist?(base_dir+key.to_s.downcase)
-	file = File.new(base_dir+key.to_s.downcase+"/index.html","a+")
+	file = File.new(base_dir+key.to_s.downcase+"/index.html","w+")
 	file << output_html
 	file.close
 end
